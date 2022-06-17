4f887c03d11e038b77374d64077f512d0d04877c
AdrianCes <aceslopez@danielcastelao.org>
2022-06-17 03:00:18 +0200

    Agregamos commit en rama lider

--- java/src/Principal.java
+++ java/src/Principal.java
@@ -2,6 +2,7 @@ public class Principal {
     public static void main(String[] args) {
             //Commit 1 rama main
             //Commit 1 rama colaborador
+            //Commit 1 rama lider
 
     }
 }
